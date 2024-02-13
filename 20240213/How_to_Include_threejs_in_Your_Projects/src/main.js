a8eefb68728419ce861c3d39c258846b316c7659
DESKTOP-MA9H283\NEWKL <devbaek77@gmail.com>
2024-02-13 13:34:01 +0900

    first commit

---
+++ 20240213/How_to_Include_threejs_in_Your_Projects/src/main.js
@@ -0,0 +1,15 @@
+// import three.js here
+// import * as THREE from "./vendor/three/build/three.module.js";
+
+// THREE.PerspectiveCamera;
+// THREE.MeshStandardMaterial;
+// THREE.Texture;
+
+// import { 
+//     PerspectiveCamera,
+//     MeshStandardMaterial,
+//     WebGLRenderer,
+// } from './vendor/three/build/three.module.js';
+
+import { OrbitControls } from 'three';
+
