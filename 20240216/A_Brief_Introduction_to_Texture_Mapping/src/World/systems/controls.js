7ccc9bd333a45ba31815a0048b3edc0830c31001
DESKTOP-MA9H283\NEWKL <devbaek77@gmail.com>
2024-02-16 18:35:51 +0900

    cameracontrol

--- 20240216/A_Brief_Introduction_to_Texture_Mapping/src/World/systems/controls.js
+++ 20240216/A_Brief_Introduction_to_Texture_Mapping/src/World/systems/controls.js
@@ -1,10 +1,12 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const createControls = (camera, canvas) => {
+const createControls = (camera, canvas,renderer) => {
     const controls = new OrbitControls(camera, canvas);
 
+    controls.enableDamping = true;
+
     controls.tick = () => controls.update();
-    
+
     return controls;
 }
 
